e7cc099b8d6142e6ab71e2237800847279822f44
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-12 00:34:18 +0500

    add game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getNameAndGreetUser();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+
+        Scanner sc = new Scanner(System.in);
+        String userChoice = sc.nextLine();
+
+        switch (userChoice) {
+            case "1":
+                Cli.getNameAndGreetUser();
+                break;
+            case "2":
+                EvenGame.startGame();
+                break;
+            case "0":
+                break;
+            default:
+                break;
+        }
     }
 }
