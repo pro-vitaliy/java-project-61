a8381bb39cb9e965fd6424e7022627b25c644022
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-13 01:33:06 +0500

    update App.java: add game Calc to menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
 import hexlet.code.games.EvenGame;
-
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +9,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
 
@@ -22,6 +23,8 @@ public class App {
             case "2":
                 EvenGame.startEvenGame();
                 break;
+            case "3":
+                Calc.startCalcGame();
             case "0":
                 break;
             default:
