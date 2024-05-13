316b2fd14659dfd408b38986f6902d4e2264e1eb
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-13 20:56:26 +0500

    refactoring Calc.java: eliminating code duplication

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -12,23 +12,28 @@ public class Calc {
             var firstOperand = random.nextInt(20);
             var secondOperand = random.nextInt(20);
             var operatorIndex = random.nextInt(3);
+            int resOfExpression = 0;
+            String strOperator = "";
+
             switch (operatorIndex) {
                 case 0:
-                    quesAnswersDict[i][0] = firstOperand + " + " + secondOperand;
-                    quesAnswersDict[i][1] = String.valueOf(firstOperand + secondOperand);
+                    strOperator = "+";
+                    resOfExpression = firstOperand + secondOperand;
                     break;
                 case 1:
-                    quesAnswersDict[i][0] = firstOperand + " - " + secondOperand;
-                    quesAnswersDict[i][1] = String.valueOf(firstOperand - secondOperand);
+                    strOperator = "-";
+                    resOfExpression = firstOperand - secondOperand;
                     break;
                 case 2:
-                    quesAnswersDict[i][0] = firstOperand + " * " + secondOperand;
-                    quesAnswersDict[i][1] = String.valueOf(firstOperand * secondOperand);
+                    strOperator = "*";
+                    resOfExpression = firstOperand * secondOperand;
                     break;
                 default:
                     break;
             }
 
+            quesAnswersDict[i][0] = String.format("%d %s %d", firstOperand, strOperator, secondOperand);
+            quesAnswersDict[i][1] = String.valueOf(resOfExpression);
         }
 
         var mission = "What is the result of the expression?";
