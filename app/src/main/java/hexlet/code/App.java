7031715242afaf673d172b1eedb4cbc700084068
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-13 20:57:17 +0500

    add GCD game to menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.EvenGame;
+import hexlet.code.games.GCD;
+
 import java.util.Scanner;
 
 public class App {
@@ -10,6 +12,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
 
@@ -25,6 +28,10 @@ public class App {
                 break;
             case "3":
                 Calc.startCalcGame();
+                break;
+            case "4":
+                GCD.startGcdGame();
+                break;
             case "0":
                 break;
             default:
