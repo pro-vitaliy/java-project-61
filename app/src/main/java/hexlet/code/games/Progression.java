ed9ba5d6829360431029f0a5c4cd24d786181b48
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-15 22:55:43 +0500

    fix variable names

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,19 +4,19 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Progression {
-    private static Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     public static int getLenOfProgression() {
         var maxLen = 10;
         var minLen = 5;
-        return random.nextInt((maxLen - minLen) + 1) + minLen;
+        return RANDOM.nextInt((maxLen - minLen) + 1) + minLen;
     }
 
     public static int[] getProgression() {
         var length = getLenOfProgression();
         int[] progression = new int[length];
-        progression[0] = random.nextInt(30);
-        var step = random.nextInt(10) + 1;  //Для исключения выпадения шага 0 прибавим единицу
+        progression[0] = RANDOM.nextInt(30);
+        var step = RANDOM.nextInt(10) + 1;  //Для исключения выпадения шага 0 прибавим единицу
 
         for (var i = 1; i < length; i++) {
             progression[i] = progression[i - 1] + step;
@@ -24,17 +24,17 @@ public class Progression {
         return progression;
     }
 
-    public static String getQuestion(int[] sequence, int number) {
-        String strSequence = "";
+    public static String getStrProgression(int[] sequence, int number) {
+        StringBuilder strProgression = new StringBuilder();
 
-        for (int j : sequence) {
-            if (j == number) {
-                strSequence += ".. ";
+        for (int elem : sequence) {
+            if (elem == number) {
+                strProgression.append(".. ");
             } else {
-                strSequence += j + " ";
+                strProgression.append(elem).append(" ");
             }
         }
-        return strSequence;
+        return strProgression.toString();
     }
 
     public static void startProgressionGame() {
@@ -42,10 +42,10 @@ public class Progression {
 
         for (var i = 0; i < quesAnswersDict.length; i++) {
             var progression = getProgression();
-            var secretNumIndex = random.nextInt(progression.length);
+            var secretNumIndex = RANDOM.nextInt(progression.length);
             var secretNum = progression[secretNumIndex];
 
-            quesAnswersDict[i][0] = getQuestion(progression, secretNum);
+            quesAnswersDict[i][0] = getStrProgression(progression, secretNum);
             quesAnswersDict[i][1] = String.valueOf(secretNum);
         }
 
