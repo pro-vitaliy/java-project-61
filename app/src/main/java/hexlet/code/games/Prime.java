4ff5faa139dd1e30d594c05e9154476b37e31ad6
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-16 20:35:50 +0500

    fix Prime.java adding check for numbers less than 2

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -11,7 +11,10 @@ public class Prime {
     }
 
     public static boolean isPrime(int number) {
-        for (var i = (number - 1); i > 1; i--) {
+        if (number < 2) {
+            return false;
+        }
+        for (var i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
             }
