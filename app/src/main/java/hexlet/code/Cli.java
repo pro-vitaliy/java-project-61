574f9016f688acc07936bf71581a158c10d0c665
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-16 20:37:22 +0500

    fix cli.java change println to print, remove return

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String getNameAndGreetUser() {
+    public static void getNameAndGreetUser() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.print("May I have your name? ");
         String userName = sc.nextLine();
         System.out.println("Hello, " + userName + "!");
-
-        return userName;
     }
 }
