85cbad67e037c6bb766b2ba29fc6924bf6bff507
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-18 19:42:06 +0500

    update App.java: refactor switch to modern arrow syntax

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,16 +9,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
-
+        System.out.print("""
+            Please enter the game number and press Enter.
+            1 - Greet
+            2 - Even
+            3 - Calc
+            4 - GCD
+            5 - Progression
+            6 - Prime
+            0 - Exit
+            Your choice:\s""");
         Scanner sc = new Scanner(System.in);
         String userChoice = sc.nextLine();
         startGame(userChoice);
@@ -26,25 +26,15 @@ public class App {
 
     public static void startGame(String choice) {
         switch (choice) {
-            case "1":
-                Cli.getNameAndGreetUser();
-                break;
-            case "2":
-                EvenGame.startEvenGame();
-                break;
-            case "3":
-                Calc.startCalcGame();
-                break;
-            case "4":
-                GCD.startGcdGame();
-                break;
-            case "5":
-                Progression.startProgressionGame();
-                break;
-            case "6":
-                Prime.startPrimeGame();
-            default:
-                break;
+            case "1" -> Cli.getNameAndGreetUser();
+            case "2" -> EvenGame.startEvenGame();
+            case "3" -> Calc.startCalcGame();
+            case "4" -> GCD.startGcdGame();
+            case "5" -> Progression.startProgressionGame();
+            case "6" -> Prime.startPrimeGame();
+            default -> {
+                // Exit
+            }
         }
     }
 }
