dac2a5bf54e309141770086f6fe2161c367c43a8
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-19 02:14:45 +0500

    refactor Even.java: Extracted even number check into separate method

---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,22 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import hexlet.code.Utils;
+
+public class Even {
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static void start() {
+        String[][] quesAnswersDict = new String[Engine.MAX_ROUNDS][];
+        for (var i = 0; i < quesAnswersDict.length; i++) {
+            final int maxNum = 100;
+            var number = Utils.generateNum(maxNum);
+            var answer = isEven(number) ? "yes" : "no";
+            quesAnswersDict[i] = new String[] {String.valueOf(number), answer};
+        }
+        var mission = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        Engine.runGame(mission, quesAnswersDict);
+    }
+}
