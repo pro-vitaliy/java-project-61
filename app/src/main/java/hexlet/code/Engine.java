fb626b211966e1e3564b2233e239d4431fc42e3d
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-19 23:03:08 +0500

    refactor Engine.java: change while to for-each

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Engine {
     private static final Scanner SC = new Scanner(System.in);
+    public static final int MAX_ROUNDS = 3;
 
     public static String getNameAndGreetUser() {
         System.out.println("Welcome to the Brain Games!");
@@ -13,30 +14,25 @@ public class Engine {
         return userName;
     }
 
-    public static void checkUserAnswer(String task, String[][] quesAndAnswers) {
+    public static void runGame(String task, String[][] quesAndAnswers) {
         var userName = getNameAndGreetUser();
         System.out.println(task);
-        var roundCount = 0;
-        var maxRoundCount = quesAndAnswers.length;
 
-        while (roundCount < maxRoundCount) {
-            var question = quesAndAnswers[roundCount][0];
-            var correctAnswer = quesAndAnswers[roundCount][1];
+        for (var dict : quesAndAnswers) {
+            var question = dict[0];
+            var correctAnswer = dict[1];
             System.out.println("Question: " + question);
             System.out.print("Your answer: ");
             var userAnswer = SC.nextLine();
             if (userAnswer.equals(correctAnswer)) {
                 System.out.println("Correct!");
-                roundCount++;
             } else {
                 System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.%n", userAnswer, correctAnswer);
                 System.out.printf("Let's try again, %s!%n", userName);
-                break;
+                return;
             }
         }
-        if (roundCount == maxRoundCount) {
-            System.out.printf("Congratulations, %s!%n", userName);
-        }
+        System.out.printf("Congratulations, %s!%n", userName);
         SC.close();
     }
 }
