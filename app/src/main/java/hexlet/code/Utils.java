23f778e7a7ee7e5b97fac51f7a47ba33c1bbfdd9
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-19 22:59:15 +0500

    update Utils.java add method generateNum

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -7,14 +7,4 @@ public class Utils {
         Random random = new Random();
         return random.nextInt(bound);
     }
-
-    public static int[] generateProgression(int minLength, int maxLength, int lowerBound, int step) {
-        var length = generateNum((maxLength - minLength) + 1) + minLength;
-        int[] progression = new int[length];
-        progression[0] = generateNum(lowerBound);
-        for (var i = 1; i < length; i++) {
-            progression[i] = progression[i - 1] + step;
-        }
-        return progression;
-    }
 }
