e7b677df9f5e0542989da67008a7b97a12939a73
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-22 22:52:27 +0500

    refactoring Progression.java add generateProgression method

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,39 +4,48 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Progression {
-    public static String[] genProgressionAndHideElem() {
-        final int minLength = 5;
-        final int maxLength = 10;
-        final int maxFirstElem = 30;
-        final int maxStep = 10;
-
-        int length = Utils.generateNum((maxLength - minLength) + 1) + minLength;
-        int firstElem = Utils.generateNum(maxFirstElem);
-        int step = Utils.generateNum(maxStep) + 1;
+    public static int[] generateProgression(int length, int firstNum, int step) {
         int[] progression = new int[length];
         for (var i = 0; i < length; i++) {
-            progression[i] = firstElem + i * step;
+            progression[i] = firstNum + i * step;
         }
-        int hiddenElemIndex = Utils.generateNum(length);
-        int hiddenElem = progression[hiddenElemIndex];
+        return progression;
+    }
 
-        StringBuilder strProgression = new StringBuilder();
+    public static String formatProgression(int[] progression, int number) {
+        StringBuilder formattedProgression = new StringBuilder();
         for (int elem : progression) {
-            if (elem == hiddenElem) {
-                strProgression.append(".. ");
+            if (elem == number) {
+                formattedProgression.append(".. ");
             } else {
-                strProgression.append(elem).append(" ");
+                formattedProgression.append(elem).append(" ");
             }
         }
-        return new String[] {strProgression.toString(), String.valueOf(hiddenElem)};
+        return String.valueOf(formattedProgression);
     }
 
     public static void start() {
         String[][] quesAnswersDict = new String[Engine.MAX_ROUNDS][];
 
         for (var i = 0; i < quesAnswersDict.length; i++) {
-            quesAnswersDict[i] = genProgressionAndHideElem();
+            final int minLength = 5;
+            final int maxLength = 10;
+            final int maxFirstElem = 30;
+            final int maxStep = 10;
+
+            int length = Utils.generateNum((maxLength - minLength) + 1) + minLength;
+            int firstElem = Utils.generateNum(maxFirstElem);
+            int step = Utils.generateNum(maxStep) + 1;
+            int [] progression = generateProgression(length, firstElem, step);
+
+            int hiddenElemIndex = Utils.generateNum(length);
+            int hiddenElem = progression[hiddenElemIndex];
+
+            String question = formatProgression(progression, hiddenElem);
+            String answer = String.valueOf(hiddenElem);
+            quesAnswersDict[i] = new String[] {question, answer};
         }
+
         var mission = "What number is missing in the progression?";
         Engine.runGame(mission, quesAnswersDict);
     }
