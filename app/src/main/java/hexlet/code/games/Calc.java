de3d7c37bab6a5cbbf7fa85b6c8d9bdeb82ce535
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-05-22 22:51:29 +0500

    refactoring Calc.java extracted evaluating expression into separate method

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,31 +4,31 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Calc {
-    public static String[] genExpressionAndResult() {
-        final int maxOperand = 20;
-        int firstOperand = Utils.generateNum(maxOperand);
-        int secondOperand = Utils.generateNum(maxOperand);
-
-        String[] operators = new String[] {"+", "-", "*"};
-        var operatorIndex = Utils.generateNum(operators.length);
-        String operator = operators[operatorIndex];
-
-        int resultOfExpression = switch (operator) {
+    public static int evaluateExpression(int firstOperand, int secondOperand, String operator) {
+        return switch (operator) {
             case "+" -> firstOperand + secondOperand;
             case "-" -> firstOperand - secondOperand;
             case "*" -> firstOperand * secondOperand;
             default -> throw new IllegalStateException("Unexpected value: " + operator);
         };
-        String expression = String.format("%d %s %d", firstOperand, operator, secondOperand);
-        String result = String.valueOf(resultOfExpression);
-        return new String[] {expression, result};
     }
 
     public static void start() {
         String[][] quesAnswersDict = new String[Engine.MAX_ROUNDS][];
 
         for (var i = 0; i < quesAnswersDict.length; i++) {
-            quesAnswersDict[i] = genExpressionAndResult();
+            final int maxOperand = 20;
+            int firstOperand = Utils.generateNum(maxOperand);
+            int secondOperand = Utils.generateNum(maxOperand);
+
+            String[] operators = new String[] {"+", "-", "*"};
+            var operatorIndex = Utils.generateNum(operators.length);
+            String operator = operators[operatorIndex];
+            int resultOfExpression = evaluateExpression(firstOperand, secondOperand, operator);
+
+            String question = String.format("%d %s %d", firstOperand, operator, secondOperand);
+            String answer = String.valueOf(resultOfExpression);
+            quesAnswersDict[i] = new String[] {question, answer};
         }
         var mission = "What is the result of the expression?";
         Engine.runGame(mission, quesAnswersDict);
